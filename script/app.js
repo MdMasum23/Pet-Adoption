d3764b6aacd1ddb6ee64c2d7b4b06fbb1b5481a5
MdMasum23 <mdmasum67172019@gmail.com>
2025-08-20 19:08:47 +0600

    display categories dynamically with buttons and icons

--- script/app.js
+++ script/app.js
@@ -8,15 +8,20 @@ const loadCategory = async () => {
 }
 
 
+const showCategory = (categories) => {
+    categories.forEach((element) => {
+        const categoryContainer = document.getElementById('category-container');
+        const div = document.createElement("div");
+        div.innerHTML = `
+        <button onclick="loadPets('${element.category}')" class="btn">${element.category} 
+        <img class="w-8" src=${element.category_icon} alt="" />
+        </button>
+        `
+        categoryContainer.appendChild(div)
 
-
-loadPets("cat")
-
-
-
-loadCategory()
-
-
+    })
+}
+``
 
 
 
